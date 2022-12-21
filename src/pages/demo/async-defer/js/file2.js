c337af42969c1f8c93468a535f0dfc2cbf5c56cd
zhenghuahou <2430370966@qq.com>
2022-12-21 15:29:19 +0800

    feat: add async-defer demo

---
+++ src/pages/demo/async-defer/js/file2.js
@@ -0,0 +1,6 @@
+console.log('File 2 executing');
+
+var output2 = document.getElementById('output2');
+output2.innerText = 'Welcome message from JS - File 2';
+
+// NO Random text to increase file size
