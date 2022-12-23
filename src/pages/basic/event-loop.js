89a53ad0a010a4c2e194d4c61e086c61d94a2f55
zhenghuahou <2430370966@qq.com>
2022-12-23 19:32:30 +0800

    feat: modify event loop  demo

--- src/pages/basic/event-loop.js
+++ src/pages/basic/event-loop.js
@@ -3,28 +3,35 @@ import Highlight from '../../components/highlight';
 export default function EventLoop() {
 
     const code = `
-        console.log(1);
+    console.log(1);
 
-        setTimeout(() => console.log(2));
+    setTimeout(() => console.log(2));
 
-        Promise.resolve().then(() => console.log(3)).then(() => console.log('add 1')).then(()=>console.log('add 1*'));
+    Promise.resolve().then(() => console.log(3)).then(() => console.log('add 1')).then(()=>console.log('add 1*'));
 
-        Promise.resolve().then(() => setTimeout(() => console.log(4)));
+    Promise.resolve().then(() => setTimeout(() => console.log(4)));
 
-        Promise.resolve().then(() => console.log(5)).then(() => console.log('add 2'));
+    Promise.resolve().then(() => console.log(5)).then(() => console.log('add 2'));
 
-        Promise.resolve().then(() => console.log('then test'));
-        setTimeout(() => console.log(6));
+    Promise.resolve().then(() => console.log('then test'));
+    setTimeout(() => console.log(6));
 
-        console.log(7)
+    console.log(7)
 
-        // 1 7 3 5 'then test'  'add 1' 'add 2' 'add 1*' 2  6 4
+    // 1 7 3 5 'then test'  'add 1' 'add 2' 'add 1*' 2  6 4
     `
 
     return (
         <>
             <a href='https://zh.javascript.info/event-loop'>详细解题网址</a><br />
             <a href='https://time.geekbang.org/column/article/135624'>宏任务和微任务</a>
+            <br />
+            <p>
+                    macrotasks: setTimeout, setInterval, setImmediate, requestAnimationFrame, I/O, UI rendering
+            </p>
+            <p>
+                    microtasks: process.nextTick, Promises, queueMicrotask, MutationObserver
+            </p>
             <hr />
             <h3>event loop demo</h3>
             <Highlight className="code">
