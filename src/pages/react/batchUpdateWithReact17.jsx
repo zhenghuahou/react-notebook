b7f19de9085681bb9fc0bbd2b3d5186199afcb46
zhenghuahou <2430370966@qq.com>
2023-03-04 16:18:09 +0800

    feat: add batchUpdate

--- src/pages/react/batchUpdateWithReact17.jsx
+++ src/pages/react/batchUpdateWithReact17.jsx
@@ -1,5 +1,6 @@
 import { useState, useLayoutEffect } from "react";
 import * as ReactDOM from "react-dom";
+import { unstable_batchedUpdates, flushSync } from "react-dom";
 
 // https://juejin.cn/post/7153814771937067044
 
@@ -12,17 +13,33 @@ function App() {
     //React 17 render 1次
     setCount((c) => c + 1);
     setFlag((f) => !f);
+
     // fetchSomething().then(() => {
     //   // React 17 render 2次
     //   // React 17 and earlier does NOT batch these:
     //   setCount((c) => c + 1); // Causes a re-render
     //   setFlag((f) => !f); // Causes a re-render
     // });
+
     // setTimeout(()=>{
     //   // React 17 render 2次
     //   setCount((c) => c + 1); // Causes a re-render
     //   setFlag((f) => !f); // Causes a re-render
     // })
+
+    //更新一次，批量更新
+    // unstable_batchedUpdates(() => {
+    //   setCount((c) => c + 1);
+    //   setFlag((f) => !f);
+    // });
+
+    //更新2次
+    // flushSync(() => {
+    //   setCount((c) => c + 1);
+    // });
+    // flushSync(() => {
+    //   setFlag((f) => !f);
+    // });
   }
 
   return (
