3ca19a264d1816cb385f4abb8503ef24f687e1ab
zhenghuahou <2430370966@qq.com>
2023-03-15 11:41:04 +0800

    feat: koa-compose的实现

--- src/pages/node/koa/compose.js
+++ src/pages/node/koa/compose.js
@@ -1,7 +1,34 @@
+function compose(middleware) {
+  return function (context) {
+    let index = -1;
+    let rst;
+    function dispatch(i, ctx) {
+      // console.info("i:", i, "index:", index, " ctx:", ctx);
+      if (i <= index)
+        return Promise.reject(new Error("next() called multiple times"));
+      index = i;
+      let fn = middleware[i];
+      if (i <= middleware.length - 1) {
+        return Promise.resolve(fn(ctx, dispatch.bind(null, i + 1)));
+      } else if (i > middleware.length - 1) {
+        // console.info("fn:", fn);
+        rst = ctx;
+        return Promise.resolve(ctx);
+      }
+    }
+
+    const m0 = dispatch(0, context);
+    console.info(" m0:", m0);
+    return Promise.resolve(rst);
+  };
+}
+
+/* 以下为测试代码 */
+
 function num(ctx, next) {
   console.info(">>>开始计算10件");
   next(ctx * 10);
-  //   next(ctx * 4);
+  // next(ctx * 4);
   console.info("<<<10件计算结束");
   return "num";
 }
@@ -16,30 +43,6 @@ function calculate(ctx, next) {
   console.info(">>>计算邮费开始", next, " ctx:", ctx);
   next(ctx + 12);
   console.info("<<<计算邮费结束");
-}
-
-function compose(middleware) {
-  return function (context, next) {
-    let index = -1;
-    let i = 0;
-    let rst;
-    function dispatch(i, ctx) {
-      if (i <= index) return new Error("next() called multiple times");
-      index = i;
-      let fn = middleware[i];
-      if (i <= middleware.length - 1) {
-        return fn(ctx, dispatch.bind(null, i + 1));
-      } else if (i > middleware.length - 1) {
-        // console.info("fn:", fn);
-        rst = ctx;
-        return ctx;
-      }
-    }
-
-    const m0 = dispatch(0, context);
-    console.info(" m0:", m0);
-    return rst;
-  };
 }
 
 const pipe = compose([num, discount, calculate]);
