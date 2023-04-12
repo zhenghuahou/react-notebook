251a8efbb09524b12283ac21011fd1824a477196
zhenghuahou <2430370966@qq.com>
2023-04-12 18:26:52 +0800

    feat: add deepcopy + shuffle

--- src/pages/program/shuffle.js
+++ src/pages/program/shuffle.js
@@ -1,12 +1,4 @@
 /* eslint-disable no-prototype-builtins */
-/*
- * @Author: zhenghuahou 2430370966@qq.com
- * @Date: 2023-04-12 15:55:55
- * @LastEditors: zhenghuahou 2430370966@qq.com
- * @LastEditTime: 2023-04-12 18:20:11
- * @FilePath: /fe-notebook/src/pages/program/deepcopy.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import Highlight from "../../components/highlight";
 export default function ArrayFlat() {
   const code = `
